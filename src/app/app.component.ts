d2c39ca74432ac78da5b6c94c117c7c1ab238dc0
taras.schevchuk <taras.schevchuk@amedial.com>
2024-12-07 19:56:03 +0100

    temp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core'
 import { RouterLink, RouterOutlet } from '@angular/router'
 import { TranslocoRootModule } from './transloco.root.module'
 import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TranslocoRootModule, RouterLink],
+  imports: [CommonModule, RouterOutlet, TranslocoRootModule, RouterLink, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  mailForm!: FormGroup
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initializeForm()
+  }
+
+  initializeForm() {
+    this.mailForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: [''],
+      body: ['', Validators.required],
+    })
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('Form Data:', form.value)
+    this.mailForm.reset()
+  }
 }
